ee007642aec6c8512c1eac1a15bea28a5e3a39a7
damoncrockett <damoncrockett@hotmail.com>
2020-12-16 18:44:47 -0500

    basic vis code
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import Scatter from './Scatter';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { // global state
+      model: 'umap',
+      data: null
+    };
+
+    this.handleModel = this.handleModel.bind(this);
+  }
+
+
+  handleModel() {
+    let modelname = this.state.model;
+    if (modelname==='umap') {
+      modelname = 'tsne';
+    } else if (modelname==='tsne') {
+      modelname = 'umap';
+    }
+
+    this.setState(state => ({
+      model: modelname
+    }));
+  }
+
+  getData() {
+    fetch('http://localhost:8888/'+this.state.model+'.json')
+      .then(response => response.json())
+      .then(data => this.setState(state => ({
+        data: data)
+      })));
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // conditional prevents infinite loop from render to cDU
+    if (prevState.model !== this.state.model) {
+      this.setState(state => ({
+        data: this.state.data //process w no re-fetch
+      }))
+    }
+  }
+
+  render() {
+    const bkgd = '#e8e3cd';
+    const stroke = '#9f9a86';
+
+    const modelStyle = {
+      backgroundColor: bkgd,
+      color: stroke,
+    };
+
+    return (
+      <div className='app'>
+        <div className='field'>
+          <Scatter
+            data={this.state.data}
+          />
+        </div>
+        <div className='panel'>
+          <div className='buttonStrip'>
+            <button onClick={this.handleModel} style={modelStyle}>MODEL</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Scatter.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { select } from 'd3-selection';
+
+const margin = {top: 40, right: 40, bottom: 40, left: 40};
+
+class Scatter extends Component {
+  constructor(props) {
+    super(props);
+    this.drawScatter = this.drawScatter.bind(this);
+    this.svgNode = React.createRef();
+    this.state = {
+      plotH: 1000,
+      plotW: 1000,
+      svgW: plotW + margin.left + margin.right,
+      svgH: plotH + margin.top + margin.bottom
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // conditional prevents infinite loop
+    if (prevProps.data !== this.props.data) {
+      this.drawScatter();
+    }
+  }
+
+  drawScatter() {
+    const svgNode = this.svgNode.current;
+
+    select(svgNode)
+      .selectAll('g.plotCanvas')
+      .data([0]) // bc enter selection, prevents appending new 'g' on re-render
+      .enter()
+      .append('g')
+      .attr('class', 'plotCanvas') // purely semantic
+      .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    // This selection is non-empty only the first time
+    select(svgNode)
+      .select('g.plotCanvas')
+      .selectAll('image')
+      .data(this.props.data)
+      .enter()
+      .append('image')
+
+    select(svgNode)
+      .select('g.plotCanvas')
+      .selectAll('image')
+      .data(this.props.data)
+      .attr('width', 100 )
+      .attr('height', 100 )
+      .attr('x', d => d.x * 1000 )
+      .attr('y', d => d.y * 1000 )
+      .attr('xlink:href', d => d.imgpath )
+
+      ));
+
+      //window.scrollTo( 0, this.state.svgH );
+    }
+
+  render() {
+    return <svg
+             ref={this.svgNode}
+             width={this.state.svgW}
+             height={this.state.svgH}
+           />;
+  }
+}
+
+export default Scatter;
